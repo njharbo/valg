5328743f86310e8adb9baebcce6f9cf43b44a597
Niels-Jakob Harbo <njharbo@gmail.com>
2015-06-30 23:15:33 +0200

    Changed border to 90 from 89.5

--- compute_majorities.m
+++ compute_majorities.m
@@ -16,20 +16,20 @@ numberparties=length(mandater);
 border=90;
 
 %Form all possible coalitions
-nj=zeros(1,numberparties)
+koalitions=zeros(1,numberparties)
 n=1
-nj(1:9,1)=nchoosek([1:1:numberparties], n)
+koalitions(1:9,1)=nchoosek([1:1:numberparties], n)
 for n=2:1:numberparties-1
-dim1=length(nj(:,1))+1;
+dim1=length(koalitions(:,1))+1;
 dim2=length(nchoosek([1:1:numberparties], n))+dim1-1;
-nj(dim1:dim2,1:n)=nchoosek([1:1:numberparties], n)
+koalitions(dim1:dim2,1:n)=nchoosek([1:1:numberparties], n)
 end
-%Put all coaltions on binary form
-koalitionsvektor=zeros(length(nj),length(nj(1,:)));
-for i=1:length(nj)
-    for k=1:length(nj(1,:))
-        if nj(i, k)>0
-        koalitionsvektor(i, nj(i, k))=1;
+%Put all coalitions on binary form
+koalitionsvektor=zeros(length(koalitions),length(koalitions(1,:)));
+for i=1:length(koalitions)
+    for k=1:length(koalitions(1,:))
+        if koalitions(i, k)>0
+        koalitionsvektor(i, koalitions(i, k))=1;
         end
     end
 end
@@ -47,7 +47,7 @@ temp=transpose(koalitionsvektor(i,1:9)).*mandater;
 smallest=min(temp(temp>0));
 koalitionsvektor(i,11)=koalitionsvektor(i,10)-smallest;
 end
-koalitionsvektor=koalitionsvektor(koalitionsvektor(:,11)<=border,:)
+koalitionsvektor=koalitionsvektor(koalitionsvektor(:,11)<border,:)
 
 koalitionsvektor=koalitionsvektor(:,1:10);
 
